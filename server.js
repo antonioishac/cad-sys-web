a0f282d49eb5a34fd280c4a7cac2e055444e5089
Antonio Ishac <antonioishac@gmail.com>
2018-11-19 02:38:30 -0200

    mudanca

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/cad-sys-web'));
 
 app.get('/*', function(req, res) {
   res.sendFile(__dirname + '/dist/cad-sys-web/index.html');
